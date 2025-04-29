bd59bc7dc5f4aa1e9fd50667ca14bfa7a62c2b79
Danylo Shchetinin <mail.redoge@gmail.com>
2025-04-29 23:47:19 +0300

    Update response url

--- controllers/iframes.js
+++ controllers/iframes.js
@@ -7,13 +7,14 @@ const getApiKeyIframeController = (req, res) => {
 }
 const getApiKeyIframeLinkController = (req, res) => {
     const url = `${process.env.APP_URL}/iframes/apikey`
-    res.json({iframeUrl:url});
+    const response = { iframeUrl: url }
+    res.json({ response });
 }
-const postApiKeyIframeController = (req, res)=>{
+const postApiKeyIframeController = (req, res) => {
     const {body, params, query, headers} = req;
     res.json({body, params, query, headers});
 }
 
 
 // First req: ?actionType=IFRAME_FETCH&portalId=146088390&userId=79601114&userEmail=danylo.shchetinin@streamtele.com&appId=11511621&accountId=joedoe@example.com
-module.exports = {getApiKeyIframeController, getApiKeyIframeLinkController,postApiKeyIframeController}
+module.exports = {getApiKeyIframeController, getApiKeyIframeLinkController, postApiKeyIframeController}
