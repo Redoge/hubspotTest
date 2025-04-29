aeef7e755b3bddc44874b98788b2d2b8a052b0e1
Danylo Shchetinin <mail.redoge@gmail.com>
2025-04-29 23:51:50 +0300

    feat(iframes): enhance API key iframe functionality
    - Added additional query parameters to iframes routes
    - Included portalId, userId, userEmail, appId, and accountId in iframe rendering
    - Updated iframe link generation to include all query parameters
    - Simplified postApiKeyIframeController response

--- controllers/iframes.js
+++ controllers/iframes.js
@@ -1,20 +1,21 @@
 const getApiKeyIframeController = (req, res) => {
+    const {portalId, userId, userEmail, appId, accountId} = req.query;
     const postApiKeySave = `${process.env.APP_URL}/iframes/apikey`
     const data = {
-        postApiKeySave
+        postApiKeySave, portalId, userId, userEmail, appId, accountId
     }
     res.render('iframes/apikey', data);
 }
 const getApiKeyIframeLinkController = (req, res) => {
-    const url = `${process.env.APP_URL}/iframes/apikey`
-    const response = { iframeUrl: url }
-    res.json({ response });
+    const {portalId, userId, userEmail, appId, accountId} = req.query;
+    const url = `${process.env.APP_URL}/iframes/apikey?portalId=${portalId}&userId=${userId}&userEmail=${userEmail}&appId=${appId}&accountId=${accountId}`;
+    const response = {iframeUrl: url}
+    res.json({response});
 }
 const postApiKeyIframeController = (req, res) => {
     const {body, params, query, headers} = req;
-    res.json({body, params, query, headers});
+    res.json({body, params, query});
 }
 
 
-// First req: ?actionType=IFRAME_FETCH&portalId=146088390&userId=79601114&userEmail=danylo.shchetinin@streamtele.com&appId=11511621&accountId=joedoe@example.com
 module.exports = {getApiKeyIframeController, getApiKeyIframeLinkController, postApiKeyIframeController}
