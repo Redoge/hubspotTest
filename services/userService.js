5b08d854690ee261f452eebf511e3f7752d84ece
Danylo Shchetinin <mail.redoge@gmail.com>
2025-04-30 00:54:35 +0300

    feat(auth): implement initial user service methods
    - Added `saveAuthUser` method stub for saving authenticated users
    - Added `getAuthUsers` method stub for retrieving authenticated users
    - Included example JSON comment demonstrating expected user account structure

---
+++ services/userService.js
@@ -0,0 +1,17 @@
+const saveAuthUser = () =>{}
+const getAuthUsers = () =>{}
+/*
+[
+    {
+        "accountId": "joedoe@example.com",
+        "accountName": "Joe Doe",
+        "accountLogoUrl": "https://www.hubspot.com/hubfs/assets/hubspot.com/style-guide/brand-guidelines/guidelines_condemned-sprocket-2.svg",
+        "appId": 11511621
+    },
+    {
+        "accountId": "janedoe@example.com",
+        "accountName": "Jane Doe",
+        "accountLogoUrl": "https://www.hubspot.com/hubfs/assets/hubspot.com/style-guide/brand-guidelines/guidelines_approved-sprocket-2.svg",
+        "appId": 11511621
+    }
+]*/
