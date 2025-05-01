96e6bcc9e1855ceccc342b762a3afd772fdfcac7
Danylo Shchetinin <mail.redoge@gmail.com>
2025-05-01 17:14:53 +0300

    Refactor getAuthUsers to fetch data using userService
    Updated the getAuthUsers controller to utilize the getAuthUsersByData function from userService for fetching authenticated users based on query parameters. This improves code modularity and enhances maintainability.

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,9 @@
+const {getAuthUsersByData} = require("../services/userService");
 const getAuthUsers = async (req, res) => {
-
+    const data = req.query;
+    const result = await getAuthUsersByData(data)
+    res.json({result})
 }
 
 
+module.exports = {getAuthUsers}
