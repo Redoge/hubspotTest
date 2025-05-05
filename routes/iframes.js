8e162a6eb98feafdb5e6a2576c0891b3200cfbba
Danylo Shchetinin <mail.redoge@gmail.com>
2025-05-05 17:45:06 +0300

    feat(routes): add new route for CTC card data retrieval
    - Imported `getCTCCardControllerData` from iframes controllers
    - Added new GET route `/crm-card/ctc/data` for retrieving CTC card data

--- routes/iframes.js
+++ routes/iframes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const {getApiKeyIframeController, getApiKeyIframeLinkController, postApiKeyIframeController, getCTCCardController} = require("../controllers/iframes");
+const {getApiKeyIframeController, getApiKeyIframeLinkController, postApiKeyIframeController, getCTCCardController,
+    getCTCCardControllerData
+} = require("../controllers/iframes");
 
 
 router.get('/apikey', getApiKeyIframeController);
 router.post('/apikey', postApiKeyIframeController);
 router.get('/apikey/url', getApiKeyIframeLinkController);
 router.get('/crm-card/ctc', getCTCCardController);
+router.get('/crm-card/ctc/data', getCTCCardControllerData);
 module.exports = router;
