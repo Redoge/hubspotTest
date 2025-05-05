12d70ed75020ba4e888ee2d988d76fd85beecbfb
Danylo Shchetinin <mail.redoge@gmail.com>
2025-05-05 17:37:15 +0300

    feat(routes): add CRM card endpoint for CTC
    - Added new route `/crm-card/ctc` for retrieving CTC card data
    - Imported `getCTCCardController` from iframes controllers

--- routes/iframes.js
+++ routes/iframes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const {getApiKeyIframeController, getApiKeyIframeLinkController, postApiKeyIframeController} = require("../controllers/iframes");
+const {getApiKeyIframeController, getApiKeyIframeLinkController, postApiKeyIframeController, getCTCCardController} = require("../controllers/iframes");
 
 
 router.get('/apikey', getApiKeyIframeController);
 router.post('/apikey', postApiKeyIframeController);
 router.get('/apikey/url', getApiKeyIframeLinkController);
-
+router.get('/crm-card/ctc', getCTCCardController);
 module.exports = router;
